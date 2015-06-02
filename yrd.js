f4e9c7436d56a0a90daffde20181a98ef1ff8f1e
wangzhiwei <zhiweiwang6@creditease.cn>
2015-06-02 18:42:06 +0800

    计算器

--- yrd.js
+++ yrd.js
@@ -2,7 +2,13 @@ $(function(){
 
 	$("#btn1").on("click",function(){
 
-		$("body").append('test-----------');
+		$("body").append('-----------');
 		
+		var v1 = parseInt($("#val1").val());
+		var v2 = parseInt($("#val2").val());
+
+		$("body").append(v1+v2);
+
+
 	})
 })
