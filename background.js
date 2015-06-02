2d361c18d7298137011d02b226f2edba3310c59f
wangzhiwei <zhiweiwang6@creditease.cn>
2015-06-02 18:23:34 +0800

    Desktop Capture Example

--- background.js
+++ background.js
@@ -1,12 +1,20 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    bounds: {
+      width: 700,
+      height: 600
+    }
   });
+});
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  chrome.desktopCapture.chooseDesktopMedia(
+      ["screen", "window"],
+      function(id) {
+        sendResponse({"id": id});
+      });
 });
--- popup.js
+++ popup.js
@@ -1,128 +1,51 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Search the bookmarks when entering the search keyword.
-$(function() {
-  $('#search').change(function() {
-     $('#bookmarks').empty();
-     dumpBookmarks($('#search').val());
-  });
-});
-// Traverse the bookmark tree, and print the folder and nodes.
-function dumpBookmarks(query) {
-  var bookmarkTreeNodes = chrome.bookmarks.getTree(
-    function(bookmarkTreeNodes) {
-      $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
-    });
-}
-function dumpTreeNodes(bookmarkNodes, query) {
-  var list = $('<ul>');
-  var i;
-  for (i = 0; i < bookmarkNodes.length; i++) {
-    list.append(dumpNode(bookmarkNodes[i], query));
-  }
-  return list;
-}
-function dumpNode(bookmarkNode, query) {
-  if (bookmarkNode.title) {
-    if (query && !bookmarkNode.children) {
-      if (String(bookmarkNode.title).indexOf(query) == -1) {
-        return $('<span></span>');
-      }
-    }
-    var anchor = $('<a>');
-    anchor.attr('href', bookmarkNode.url);
-    anchor.text(bookmarkNode.title);
-    /*
-     * When clicking on a bookmark in the extension, a new tab is fired with
-     * the bookmark url.
-     */
-    anchor.click(function() {
-      chrome.tabs.create({url: bookmarkNode.url});
-    });
-    var span = $('<span>');
-    var options = bookmarkNode.children ?
-      $('<span>[<a href="#" id="addlink">Add</a>]</span>') :
-      $('<span>[<a id="editlink" href="#">Edit</a> <a id="deletelink" ' +
-        'href="#">Delete</a>]</span>');
-    var edit = bookmarkNode.children ? $('<table><tr><td>Name</td><td>' +
-      '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
-      '</td></tr></table>') : $('<input>');
-    // Show add and edit links when hover over.
-        span.hover(function() {
-        span.append(options);
-        $('#deletelink').click(function() {
-          $('#deletedialog').empty().dialog({
-                 autoOpen: false,
-                 title: 'Confirm Deletion',
-                 resizable: false,
-                 height: 140,
-                 modal: true,
-                 overlay: {
-                   backgroundColor: '#000',
-                   opacity: 0.5
-                 },
-                 buttons: {
-                   'Yes, Delete It!': function() {
-                      chrome.bookmarks.remove(String(bookmarkNode.id));
-                      span.parent().remove();
-                      $(this).dialog('destroy');
-                    },
-                    Cancel: function() {
-                      $(this).dialog('destroy');
-                    }
-                 }
-               }).dialog('open');
-         });
-        $('#addlink').click(function() {
-          $('#adddialog').empty().append(edit).dialog({autoOpen: false,
-            closeOnEscape: true, title: 'Add New Bookmark', modal: true,
-            buttons: {
-            'Add' : function() {
-               chrome.bookmarks.create({parentId: bookmarkNode.id,
-                 title: $('#title').val(), url: $('#url').val()});
-               $('#bookmarks').empty();
-               $(this).dialog('destroy');
-               window.dumpBookmarks();
-             },
-            'Cancel': function() {
-               $(this).dialog('destroy');
-            }
-          }}).dialog('open');
-        });
-        $('#editlink').click(function() {
-         edit.val(anchor.text());
-         $('#editdialog').empty().append(edit).dialog({autoOpen: false,
-           closeOnEscape: true, title: 'Edit Title', modal: true,
-           show: 'slide', buttons: {
-              'Save': function() {
-                 chrome.bookmarks.update(String(bookmarkNode.id), {
-                   title: edit.val()
-                 });
-                 anchor.text(edit.val());
-                 options.show();
-                 $(this).dialog('destroy');
-              },
-             'Cancel': function() {
-                 $(this).dialog('destroy');
-             }
-         }}).dialog('open');
-        });
-        options.fadeIn();
-      },
-      // unhover
-      function() {
-        options.remove();
-      }).append(anchor);
-  }
-  var li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
-  if (bookmarkNode.children && bookmarkNode.children.length > 0) {
-    li.append(dumpTreeNodes(bookmarkNode.children, query));
-  }
-  return li;
+var incognito;
+var url;
+
+function settingChanged() {
+  var type = this.id;
+  var setting = this.value;
+  var pattern = /^file:/.test(url) ? url : url.replace(/\/[^\/]*?$/, '/*');
+  console.log(type+' setting for '+pattern+': '+setting);
+  // HACK: [type] is not recognised by the docserver's sample crawler, so
+  // mention an explicit
+  // type: chrome.contentSettings.cookies.set - See http://crbug.com/299634
+  chrome.contentSettings[type].set({
+        'primaryPattern': pattern,
+        'setting': setting,
+        'scope': (incognito ? 'incognito_session_only' : 'regular')
+      });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  dumpBookmarks();
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var current = tabs[0];
+    incognito = current.incognito;
+    url = current.url;
+    var types = ['cookies', 'images', 'javascript', 'location', 'plugins',
+                 'popups', 'notifications', 'fullscreen', 'mouselock',
+                 'microphone', 'camera', 'unsandboxedPlugins',
+                 'automaticDownloads'];
+    types.forEach(function(type) {
+      // HACK: [type] is not recognised by the docserver's sample crawler, so
+      // mention an explicit
+      // type: chrome.contentSettings.cookies.get - See http://crbug.com/299634
+      chrome.contentSettings[type] && chrome.contentSettings[type].get({
+            'primaryUrl': url,
+            'incognito': incognito
+          },
+          function(details) {
+            document.getElementById(type).disabled = false;
+            document.getElementById(type).value = details.setting;
+          });
+    });
+  });
+
+  var selects = document.querySelectorAll('select');
+  for (var i = 0; i < selects.length; i++) {
+    selects[i].addEventListener('change', settingChanged);
+  }
 });
